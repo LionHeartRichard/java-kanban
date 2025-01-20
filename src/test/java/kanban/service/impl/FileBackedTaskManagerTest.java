6a891c0ef61931b7736ae943cfc580dde105f5d4
LionHeartRichard <leonhearthrich@gmail.com>
2025-01-20 21:18:44 +0300

    Add new data in tests for FileTaskManager

--- src/test/java/kanban/service/impl/FileBackedTaskManagerTest.java
+++ src/test/java/kanban/service/impl/FileBackedTaskManagerTest.java
@@ -3,6 +3,9 @@ package kanban.service.impl;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +20,8 @@ import kanban.util.Status;
 
 public class FileBackedTaskManagerTest {
 
+	private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm dd.MM.yyyy");
+
 	private FileBackedTaskManager actual;
 
 	private String path = "/home/kerrigan_kein/yandex_praktikum/java-kanban/tmp/case0.json";
@@ -28,16 +33,27 @@ public class FileBackedTaskManagerTest {
 
 		TaskInterface root = new Epic("EPIC", "E-1", "ROOT", "description ROOT", Status.NEW);
 		TaskInterface parentA = new Epic("EPIC", "E-2", "Parent A", "description parent A", Status.NEW);
-		actual.addTask(root, parentA);
+		assertTrue(actual.addTask(root, parentA));
 
 		TaskInterface parentB = new Task("TASK", "T-3", "Parent B", "description parent B", Status.NEW);
-		actual.addTask(root, parentB);
+		assertTrue(actual.addTask(root, parentB));
 
 		TaskInterface childrenA = new Subtask("SUBTASK", "S-1", "Children A", "description children A", Status.NEW);
-		actual.addTask(parentA, childrenA);
+		assertTrue(actual.addTask(parentA, childrenA));
 
 		TaskInterface childrenA1 = new Subtask("SUBTASK", "S-2", "Children A1", "description children A1", Status.NEW);
-		actual.addTask(parentA, childrenA1);
+		assertTrue(actual.addTask(parentA, childrenA1));
+
+		TaskInterface parentB2 = new Task("Parent B-2", "description parent B-2");
+		parentB2.setStartTime(LocalDateTime.parse("10:00 20.12.2024", DATE_TIME_FORMATTER));
+		parentB2.setDuration(Duration.ofMinutes(60));
+		TaskInterface childrenB2 = new Task("children B-2", "description parent B-2");
+		childrenB2.setStartTime(LocalDateTime.parse("15:00 22.12.2024", DATE_TIME_FORMATTER));
+		childrenB2.setDuration(Duration.ofMinutes(120));
+		TaskInterface root2 = new Epic("ROOT2", "Description for ROOT-2");
+
+		assertTrue(actual.addTask(root2, parentB2));
+		assertTrue(actual.addTask(parentB2, childrenB2));
 	}
 
 	@Test
