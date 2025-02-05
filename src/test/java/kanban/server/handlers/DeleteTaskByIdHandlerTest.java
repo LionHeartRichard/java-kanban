de53a27d0b355b485e992eb998ced09e6d7c6991
LionHeartRichard <leonhearthrich@gmail.com>
2025-02-05 10:21:39 +0300

    Add code in class DeleteTaskByIdHandler

--- src/test/java/kanban/server/handlers/DeleteTaskByIdHandlerTest.java
+++ src/test/java/kanban/server/handlers/DeleteTaskByIdHandlerTest.java
@@ -1,5 +1,18 @@
 package kanban.server.handlers;
 
-public class DeleteTaskByIdHandlerTest {
+import java.io.IOException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DeleteTaskByIdHandlerTest extends SetUpServerForTests {
+
+	@DisplayName("DELETE /tasks/T-1")
+	@Test
+	@Override
+	public void test() throws IOException, InterruptedException {
+		String url = "http://localhost:8080/tasks/T-1";
+		checkRequest(1, 200, url, "", "");
+	}
 
 }
